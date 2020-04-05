1f5314d203904ac520f5bed7285fe917427127b0
visionmere <63184220+visionmere@users.noreply.github.com>
2020-04-05 18:01:57 +1000

    test

---
+++ data.js
@@ -0,0 +1,287 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.009067660005545264,
+        "pitch": 0.1519572506933713,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1906682397270849,
+          "pitch": 0.7795172236716255,
+          "rotation": 0,
+          "target": "1-study"
+        },
+        {
+          "yaw": -0.16850470757644054,
+          "pitch": 0.37872425290589184,
+          "rotation": 0,
+          "target": "3-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-study",
+      "name": "Study",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.06847060911670333,
+        "pitch": 0.2786336015176225,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3109217615700173,
+          "pitch": 0.5153174371680826,
+          "rotation": 0,
+          "target": "3-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hallway2",
+      "name": "Hallway2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.06127113845186649,
+        "pitch": 0.21102861641270465,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.5791765571462175,
+        "pitch": 0.24160285067978293,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.107200742439007,
+          "pitch": 0.4915269678374372,
+          "rotation": 0,
+          "target": "4-dinning"
+        },
+        {
+          "yaw": -1.77907494115032,
+          "pitch": 0.3890143031999358,
+          "rotation": 0,
+          "target": "1-study"
+        },
+        {
+          "yaw": 0.9366849726356676,
+          "pitch": 0.20307072176021812,
+          "rotation": 0,
+          "target": "5-living"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.43799043782023617,
+          "pitch": 0.1526540823572038,
+          "title": "Kitchen",
+          "text": "Fully equipped with Stove<div>Dishwasher</div><div>Microwave</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-dinning",
+      "name": "Dinning",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.31013158246977035,
+        "pitch": 0.28845988068801987,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45933617573695074,
+          "pitch": 0.4438484668831979,
+          "rotation": 0,
+          "target": "5-living"
+        },
+        {
+          "yaw": -2.331265268059717,
+          "pitch": 0.670632239934374,
+          "rotation": 0,
+          "target": "3-kitchen"
+        },
+        {
+          "yaw": -2.2629796324113833,
+          "pitch": 0.2994330806527543,
+          "rotation": 0,
+          "target": "1-study"
+        },
+        {
+          "yaw": -1.3331088975153218,
+          "pitch": 0.5276976055384832,
+          "rotation": 0,
+          "target": "3-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-living",
+      "name": "Living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.030816862358138408,
+        "pitch": 0.2370209538950654,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8978800971295264,
+          "pitch": 0.41965769017225796,
+          "rotation": 0,
+          "target": "4-dinning"
+        },
+        {
+          "yaw": -0.8945237413660472,
+          "pitch": 0.3252381537738174,
+          "rotation": 0,
+          "target": "3-kitchen"
+        },
+        {
+          "yaw": -0.8918903591641776,
+          "pitch": 0.24737325421939005,
+          "rotation": 0,
+          "target": "1-study"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "test2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
